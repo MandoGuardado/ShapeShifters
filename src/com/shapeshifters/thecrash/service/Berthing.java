197f1f08996c37d39879fa4d79368576733e4d02
Armando Guardado <armando.guardado.ag@gmail.com>
2022-01-26 14:47:19 -0800

    Updated Berthing class to include Map of adjacent rooms, business method that be use by outside methods to determine if there is a path in a certain direction  AB#202

--- src/com/shapeshifters/thecrash/service/Berthing.java
+++ src/com/shapeshifters/thecrash/service/Berthing.java
@@ -1,16 +1,43 @@
 package com.shapeshifters.thecrash.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Berthing {
     //Fields
+    Map<String, String> adjRooms = new HashMap<>();
+
 
     //constructors
 
+    public Berthing() {
+        adjRooms.put("Starboard", "Empty");
+        adjRooms.put("Aft", "Armory");
+        adjRooms.put("Port", "Mess Hall");
+        adjRooms.put("Forward", "Bridge");
+
+    }
+
 
     //Business Methods
-
+    public boolean isPathAvailable(String direction){
+        boolean result = false;
+        if (adjRooms.containsKey(direction)) {
+            result = true;
+        }
+        return result;
+    }
 
     //Setter and getters
-
+    public Map<String, String> getAdjRooms() {
+        return adjRooms;
+    }
 
     //toString()
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() +
+                "Adjacent rooms  =" + getAdjRooms().values();
+    }
 }
