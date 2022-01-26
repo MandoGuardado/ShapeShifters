73781e7c958772be9a594211d021edbcb908be29
Alan-SDE <apotsy8251@gmail.com>
2022-01-26 15:10:15 -0500

    Added logic to controller to look in all 4 directions, AB#225

--- src/com/shapeshifters/thecrash/controller/TheCrashApp.java
+++ src/com/shapeshifters/thecrash/controller/TheCrashApp.java
@@ -15,6 +15,99 @@ public class TheCrashApp {
         }
     }
 
+    private void lookForward(String room){
+        switch (room){
+            case "bridge":
+                System.out.println("You see the forward bulkhead in the bridge");
+                break;
+            case "berthing":
+                System.out.println("You see the forward bulkhead in the berthing");
+                break;
+            case "mess hall":
+                System.out.println("You see the forward bulkhead in the mess hall");
+                break;
+            case "armory":
+                System.out.println("You see the forward bulkhead in the armory");
+                break;
+            case "med bay":
+                System.out.println("You see the forward bulkhead in the med bay");
+                break;
+            case "engineering":
+                System.out.println("You see the forward bulkhead in engineering");
+                break;
+        }
+    }
+
+    private void lookAft(String room){
+        switch (room){
+            case "bridge":
+                System.out.println("You see the aft bulkhead in the bridge");
+                break;
+            case "berthing":
+                System.out.println("You see the aft bulkhead in the berthing");
+                break;
+            case "mess hall":
+                System.out.println("You see the aft bulkhead in the mess hall");
+                break;
+            case "armory":
+                System.out.println("You see the aft bulkhead in the armory");
+                break;
+            case "med bay":
+                System.out.println("You see the aft bulkhead in the med bay");
+                break;
+            case "engineering":
+                System.out.println("You see the aft bulkhead in engineering");
+                break;
+        }
+    }
+
+    private void lookPort(String room){
+        switch (room){
+            case "bridge":
+                System.out.println("You see the port bulkhead in the bridge");
+                break;
+            case "berthing":
+                System.out.println("You see the port bulkhead in the berthing");
+                break;
+            case "mess hall":
+                System.out.println("You see the port bulkhead in the mess hall");
+                break;
+            case "armory":
+                System.out.println("You see the port bulkhead in the armory");
+                break;
+            case "med bay":
+                System.out.println("You see the port bulkhead in the med bay");
+                break;
+            case "engineering":
+                System.out.println("You see the port bulkhead in engineering");
+                break;
+        }
+    }
+
+    private void lookStbd(String room){
+        switch (room){
+            case "bridge":
+                System.out.println("You see the starboard bulkhead in the bridge");
+                break;
+            case "berthing":
+                System.out.println("You see the starboard bulkhead in the berthing");
+                break;
+            case "mess hall":
+                System.out.println("You see the starboard bulkhead in the mess hall");
+                break;
+            case "armory":
+                System.out.println("You see the starboard bulkhead in the armory");
+                break;
+            case "med bay":
+                System.out.println("You see the starboard bulkhead in the med bay");
+                break;
+            case "engineering":
+                System.out.println("You see the starboard bulkhead in engineering");
+                break;
+        }
+    }
+
+
     //GETTERS AND SETTERS
     public boolean isGameOver() {
         return gameOver;
