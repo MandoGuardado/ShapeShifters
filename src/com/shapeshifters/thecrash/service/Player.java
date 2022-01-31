9bdde11f652ff49c28d9cdfed2f02f9ac406ddd5
Armando Guardado <armando.guardado.ag@gmail.com>
2022-01-31 10:21:10 -0800

    Update Player class and defined goToAdjacentRoom() to determine if room the player wishes to go is available, Added test to test functionality, AB#374 AB#375

--- src/com/shapeshifters/thecrash/service/Player.java
+++ src/com/shapeshifters/thecrash/service/Player.java
@@ -3,26 +3,57 @@ package com.shapeshifters.thecrash.service;
 import java.text.CollationElementIterator;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 
 public class Player {
     //Fields
     private String name;
     private int hit =  100;
     private Collection<String> items = new ArrayList<>(10);
+    private Room currentLocation;
+    private Scanner in = new Scanner(System.in);
+
 
     //constructors
-    public Player(String name) {
+    public Player(String name, Room startingRoom) {
         setName(name);
+        setCurrentLocation(startingRoom);
     }
 
 
     //Business Methods
-    //Player should be able to look around a room
-    //Player should be able to pick up items
-    //Player should be able to go to and adjacent room
-    //Extra:
-    //Player will fight with ShapeShifter
-
+    public String goToAdjacentRoom(String desiredDirection){
+        String currentRoom = this.getCurrentLocation().getName();
+        String result = currentRoom;
+        if ("Bridge".equals(currentRoom) && "aft".equals(desiredDirection)){
+            System.out.println("\nChoose 1 to go to Berthing\nChoose 2 to go to Mess Hall\n");
+            int input = in.nextInt();
+            switch (input){
+                case 1:
+                    result = "Berthing";
+                    break;
+                case 2:
+                    result = "Mess Hall";
+                    break;
+            }
+        }else if ("Engineering".equals(currentRoom) && "forward".equals(desiredDirection)){
+            System.out.println("\nChoose 1 to go to Armory\nChoose 2 to go to Med Bay\n");
+            int input = in.nextInt();
+            switch (input){
+                case 1:
+                    result = "Armory";
+                    break;
+                case 2:
+                    result = "Med Bay";
+                    break;
+            }
+        } else if (getCurrentLocation().isExitAvailable(desiredDirection)){
+            result =getCurrentLocation().getExits().get(desiredDirection);
+        } else {
+            System.out.println("You can't go in that direction");
+        }
+        return result;
+    }
 
     //Setter and getters
 
@@ -50,6 +81,13 @@ public class Player {
         this.items = items;
     }
 
+    public Room getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Room currentLocation) {
+        this.currentLocation = currentLocation;
+    }
 
     //toString()
 
--- test/com/shapeshifters/thecrash/service/PlayerTest.java
+++ test/com/shapeshifters/thecrash/service/PlayerTest.java
@@ -2,17 +2,24 @@ package com.shapeshifters.thecrash.service;
 
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
+
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PlayerTest {
 
     @Test
     public void TestToStringMethod_shouldReturnStringRepresentationOfFieldValues() {
-        Player player = new Player("Jane");
+        Map<String, String> berthingExits = new HashMap<>();
+        berthingExits.put("aft", "Armory");
+        berthingExits.put("port", "Mess Hall");
+        berthingExits.put("forward", "Bridge");
+        Room berthing = new Room("Berthing","This is were everybody sleeps", berthingExits);
+        Player player = new Player("Jane", berthing);
         System.out.println(player);
         assertEquals("PlayerPlayer name = Jane', Damage received = 100, items collected = []", player.toString());
     }
@@ -20,32 +27,57 @@ public class PlayerTest {
 
     @Test
     public void testGetName_shouldReturnValuePassedIn() {
-        Player player = new Player("John");
+        Map<String, String> berthingExits = new HashMap<>();
+        berthingExits.put("aft", "Armory");
+        berthingExits.put("port", "Mess Hall");
+        berthingExits.put("forward", "Bridge");
+        Room berthing = new Room("Berthing","This is were everybody sleeps", berthingExits);
+        Player player = new Player("John", berthing);
         assertEquals("John", player.getName());
     }
 
     @Test
     public void testGetHitValue_ShouldReturnDefaultHitValue() {
-        Player player = new Player("John");
+        Map<String, String> berthingExits = new HashMap<>();
+        berthingExits.put("aft", "Armory");
+        berthingExits.put("port", "Mess Hall");
+        berthingExits.put("forward", "Bridge");
+        Room berthing = new Room("Berthing","This is were everybody sleeps", berthingExits);
+        Player player = new Player("John", berthing);
         assertEquals(100, player.getHit());
     }
 
     @Test
     public void testGetHitValue_ShouldReturnValueOfFiftyAfterUpdate() {
-        Player player = new Player("John");
+        Map<String, String> berthingExits = new HashMap<>();
+        berthingExits.put("aft", "Armory");
+        berthingExits.put("port", "Mess Hall");
+        berthingExits.put("forward", "Bridge");
+        Room berthing = new Room("Berthing","This is were everybody sleeps", berthingExits);
+        Player player = new Player("John", berthing);
         player.setHit(50);
         assertEquals(50, player.getHit());
     }
 
     @Test
     public void testItemSizeAtInstantiation_shouldReturnSizeOfZero() {
-        Player player = new Player("John");
+        Map<String, String> berthingExits = new HashMap<>();
+        berthingExits.put("aft", "Armory");
+        berthingExits.put("port", "Mess Hall");
+        berthingExits.put("forward", "Bridge");
+        Room berthing = new Room("Berthing","This is were everybody sleeps", berthingExits);
+        Player player = new Player("John", berthing);
         assertEquals(0, player.getItems().size());
     }
 
     @Test
     public void testItemsSizeAfterAddingValues_shouldReturnValueOfTwo() {
-        Player player = new Player("John");
+        Map<String, String> berthingExits = new HashMap<>();
+        berthingExits.put("aft", "Armory");
+        berthingExits.put("port", "Mess Hall");
+        berthingExits.put("forward", "Bridge");
+        Room berthing = new Room("Berthing","This is were everybody sleeps", berthingExits);
+        Player player = new Player("John", berthing);
         Collection<String> stringList = new ArrayList<>();
         stringList.add("John");
         stringList.add("Jane");
@@ -53,5 +85,15 @@ public class PlayerTest {
         assertEquals(2, player.getItems().size());
     }
 
+    @Test
+    public void testGoingAftFromBerthing_ShouldReturnArmoryString() {
+        Map<String, String> berthingExits = new HashMap<>();
+        berthingExits.put("aft", "Armory");
+        berthingExits.put("port", "Mess Hall");
+        berthingExits.put("forward", "Bridge");
 
+        Room berthing = new Room("Berthing","This is were everybody sleeps", berthingExits);
+        Player player = new Player("John", berthing);
+        assertEquals(player.goToAdjacentRoom("aft"),"Armory");
+    }
 }
